5ed8cbbef1c7b300110f89b8f38831900ad97be7
imvm <ianmanor.dev@gmail.com>
2021-02-06 21:49:43 -0300

    Move analyzer and transformer classes to own files

---
+++ OpenTextTools/TextAnalyzer.swift
@@ -0,0 +1,69 @@
+//
+//  TextAnalyzer.swift
+//  OpenTextTools
+//
+//  Created by Ian Manor on 06/02/21.
+//
+
+import Foundation
+
+class TextAnalyzer {
+    static func characterCount(text: String) -> Int {
+        let scalars = text.unicodeScalars.compactMap { scalar in
+            CharacterSet.whitespacesAndNewlines.contains(scalar) ? nil : scalar
+        }
+
+        return String(scalars).count
+    }
+
+    private static func words(_ text: String) -> [String] {
+        text.components(separatedBy: .whitespacesAndNewlines).compactMap({ $0.isEmpty ? nil : $0 })
+    }
+
+    static func wordCount(text: String) -> Int {
+        words(text).count
+    }
+
+    static func sentenceCount(text: String) -> Int {
+        text.components(separatedBy: .whitespaces).count - 1
+    }
+
+    static func paragraphCount(text: String) -> Int {
+        text.components(separatedBy: .whitespaces).count - 1
+    }
+
+    static func syllableCount(text: String) -> Int {
+        text.components(separatedBy: .whitespaces).count - 1
+    }
+
+    static func uniqueWords(text: String) -> Int {
+        Set(words(text)).count
+    }
+
+    static func averageWordLength(text: String) -> Int {
+        let wc = wordCount(text: text)
+        guard wc > 0 else { return 0}
+        return characterCount(text: text) / wc
+    }
+
+    static func readingLevel(text: String) -> FleschKincaidReadingLevel? {
+        let words = Double(wordCount(text: text))
+        let sentences = Double(sentenceCount(text: text))
+        let syllables = Double(syllableCount(text: text))
+        guard words > 0 && sentences > 0 else { return nil }
+        let fles = 206.835 - (1.015 * (words / sentences)) - (84.6 * (syllables / words))
+        return FleschKincaidReadingLevel(fles: fles)
+    }
+
+    static func readingTime(text: String) -> TimeInterval {
+        let readingTimePerLetter = 0.05
+        let totalTime = words(text).map({ Double($0.count) * readingTimePerLetter }).reduce(0, +)
+        return totalTime
+    }
+
+    static func speakingTime(text: String) -> TimeInterval {
+        let speakingTimePerLetter = 0.05
+        let totalTime = words(text).map({ Double($0.count) * speakingTimePerLetter }).reduce(0, +)
+        return totalTime
+    }
+}
---
+++ OpenTextTools/TextTransformer.swift
@@ -0,0 +1,28 @@
+//
+//  TextTransformer.swift
+//  OpenTextTools
+//
+//  Created by Ian Manor on 06/02/21.
+//
+
+class TextTransformer {
+    static func lowercase(text: String) -> String {
+        text.lowercased()
+    }
+
+    static func uppercase(text: String) -> String {
+        text.uppercased()
+    }
+
+    static func titleCase(text: String) -> String {
+        fatalError()
+    }
+
+    static func sentenceCase(text: String) -> String {
+        fatalError()
+    }
+
+    static func applying(regex: String) -> String {
+        fatalError()
+    }
+}
