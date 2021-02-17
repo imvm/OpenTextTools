6f5c80092889c021d7cadfe504c3f4257a62e78c
imvm <ianmanor.dev@gmail.com>
2021-02-17 16:39:32 -0300

    Add ContentViewModel test

--- OpenTextToolsTests/OpenTextToolsTests.swift
+++ OpenTextToolsTests/OpenTextToolsTests.swift
@@ -9,25 +9,29 @@ import XCTest
 @testable import OpenTextTools
 
 class OpenTextToolsTests: XCTestCase {
+    var viewModel: ContentViewModel!
 
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        viewModel = ContentViewModel()
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        viewModel = nil
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testUppercaseTransform() throws {
+        viewModel.text = "hello world"
+        assertTextTransform(.applyingCase(case: .uppercase), "HELLO WORLD")
     }
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    func testLowercaseTransform() throws {
+        viewModel.text = "HELLO WORLD"
+        assertTextTransform(.applyingCase(case: .lowercase), "hello world")
     }
 
+    func assertTextTransform(_ textTransform: TextTransform, _ expectedString: String) {
+        viewModel.apply(textTransform)
+        XCTAssertEqual(viewModel.transformedText, expectedString)
+        XCTAssertTrue(viewModel.showTransforms)
+    }
 }
